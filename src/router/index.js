a5bb33209077cceb5d31dde6750a3288aef95a79
king <admin@w3c.top>
2018-01-11 15:59:42 +0800

    路由懒加载示例

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../views/HelloWorld/HelloWorld.vue'
+// import HelloWorld from '../views/HelloWorld/HelloWorld.vue'
+// 此种方式引入即可实现路由懒加载
+const HelloWorld =  () => import('../views/HelloWorld/HelloWorld.vue')
 
 Vue.use(Router)
 
